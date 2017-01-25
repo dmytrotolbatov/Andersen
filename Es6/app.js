364552838c6fb7a3cfb6253961065aa9e825b64e
DmytroTolbatov <dmytrotolbatov@gmail.com>
2017-01-25 14:31:16 +0200

    template strings #1

--- Es6/app.js
+++ Es6/app.js
@@ -295,4 +295,18 @@
 // recordBirds(new Date, "sparrow", "robin", "pterodactyl");
 // console.log(birdsSeen);
 
-//////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////TEMPLATE STRINGS///////////////////////////////////////////
+
+// const teamName = "tooling";
+// const people = [{name: "Jennie", role: "senior"},
+//     {name: "Ronald", role: "junior"},
+//     {name: "Martin", role: "senior"},
+//     {name: "Anneli", role: "junior"}];
+//
+// let message = `There are ${people.length} people on the ${teamName} team.
+// Their names are ${people[0].name}, ${people[1].name}, ${people[2].name}, ${people[3].name}.
+// ${people.length/2} of them have a ${people[0].role} role.`;
+//
+// console.log(message);
+
+///////////////////////////////////////////////////////////////////////////////////////////////
