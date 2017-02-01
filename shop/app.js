e2d56073ae88e4c717e814ccb0977e180fd84ffa
DmytroTolbatov <dmytrotolbatov@gmail.com>
2017-02-01 16:11:26 +0200

    Page is done

--- shop/app.js
+++ shop/app.js
@@ -3,12 +3,47 @@ xhr.open('GET', 'shop.JSON', false);
 xhr.send();
 
 var resultArr = [];
+var clickedId = [];
+var watchedArr = [];
+var biggestSales = [];
+var shop = document.getElementById('shop');
+var biggestSalesDiv = document.getElementById('biggestSales');
+var watchedDiv = document.getElementById('watched');
 var resultDiv = document.getElementById('result');
 var data = JSON.parse(xhr.responseText);
 var index = 0;
+var buttonMore = document.getElementById('buttonMore');
+var buttonSale = document.getElementById('buttonSale');
+var buttonWatched = document.getElementById('buttonWatched');
 
-console.log( data );
-// console.log(typeof data[0].name);
+function compare(a,b) {
+    if (a.sale > b.sale)
+        return -1;
+    if (a.sale < b.sale)
+        return 1;
+    return 0;
+}
+
+function findBiggestSales() {
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].isActiveSale) {
+            biggestSales.push(data[i]);
+        }
+    }
+    return biggestSales.sort(compare);
+}
+findBiggestSales();
+
+
+function makeSale(text) {
+    var sale = document.createElement('div');
+    sale.classList.add('sale');
+    sale.innerHTML = `-${text}%`;
+    return sale;
+}
+
+
+
 
 function createItem() {
     var item = document.createElement('div');
@@ -31,180 +66,261 @@ function makePicture(src) {
 }
 
 
-/*function giveFiveItems() {
-    var row = createRow();
-    resultDiv.appendChild(row);
-    for (var j = 0; j < 5; j++) {
-        var item = createItem();
-        var heading = document.createElement('p');
-        var price = document.createElement('p');
-        heading.innerHTML = data[j].name;
-        price.innerHTML = data[j].price;
-        row.appendChild(item);
-        item.appendChild(makePicture(data[j].picture));
-        item.appendChild(heading);
-        item.appendChild(price);
-    }
-}*/
 
 function defaultPage() {
+    resultDiv.innerHTML = '';
     index = 0;
-    for (var i = 0; i < 2; i++) {
-        var row = createRow();
-        resultDiv.appendChild(row);
-        for (var j = 0; j < 5; j++, index++){
-            var item = createItem();
-            var heading = document.createElement('p');
-            var price = document.createElement('p');
-            heading.innerHTML = data[index].name;
-            price.innerHTML = data[index].price;
-            row.appendChild(item);
-            item.appendChild(makePicture(data[index].picture));
-            item.appendChild(heading);
-            item.appendChild(price);
+    var defaultFunction =  function () {
+        for (var i = 0; i < 2; i++) {
+            var row = createRow();
+            resultDiv.appendChild(row);
+            for (var j = 0; j < 5; j++, index++){
+                if (data[index]) {
+                    var item = createItem();
+                    var heading = document.createElement('p');
+                    var price = document.createElement('h3');
+                    var hiddenId = document.createElement('h5');
+                    if (data[index].name && data[index].picture) {
+                        hiddenId.innerHTML = data[index].id;
+                        heading.innerHTML = data[index].name;
+                        price.innerHTML = data[index].price;
+                        if (data[index].isActiveSale) {
+                            var sale = makeSale(data[index].sale);
+                            item.appendChild(sale);
+                        }
+                        row.appendChild(item);
+                        item.appendChild(makePicture(data[index].picture));
+                        item.appendChild(heading);
+                        item.appendChild(price);
+                        item.appendChild(hiddenId);
+                    }else {
+                        heading.innerHTML = 'Товара больше нет';
+                        row.appendChild(item);
+                        item.appendChild(heading);
+                        item.appendChild(hiddenId);
+                    }
+                }
+            }
         }
+    };
+    defaultFunction();
 
+    buttonMore.style.display = 'block';
+    buttonMore.onclick = function () {
+        defaultFunction();
+        if (index >= 32) {
+            buttonMore.style.display = 'none';
+        }
     }
-    document.getElementById('buttonMore').style.display = 'block';
 }
 
 function makeArrayOfSearched() {
     if (document.getElementById('input').value) {
         resultArr.length = 0;
-        var searchedValue = document.getElementById('input').value;
+        var searchedValue = document.getElementById('input').value.toLowerCase();
         for (var i = 0; i < data.length; i++) {
-            if (data[i].name && data[i].name.includes(searchedValue)) {
+            if (data[i].name && data[i].name.toLowerCase().includes(searchedValue)) {
                 resultArr.push(data[i]);
             }
         }
         if (resultArr.length > 10) {
-            document.getElementById('buttonMore').style.display = 'block';
+            buttonMore.style.display = 'block';
         }else {
-            document.getElementById('buttonMore').style.display = 'none';
+            buttonMore.style.display = 'none';
         }
     }
 }
 
 function giveTenItems() {
+    document.getElementById('input').value == '' ? defaultPage() : null; // maybe extra
+
     if (document.getElementById('input').value) {
         resultDiv.innerHTML = '';
-        var searchedValue = document.getElementById('input').value;
         index = 0;
+        var makeTenItems = function () {
+            for (var i = 0; i < 2; i++){
+                row = createRow();
+                resultDiv.appendChild(row);
+                for (var j = 0; j < 5; j++, index++){
+                    if (resultArr[index]) {
+                        var item = createItem();
+                        var heading = document.createElement('p');
+                        var price = document.createElement('h3');
+                        var hiddenId = document.createElement('h5');
+                        hiddenId.innerHTML = resultArr[index].id;
+                        heading.innerHTML = resultArr[index].name;
+                        price.innerHTML = resultArr[index].price;
+                        if (resultArr[index].isActiveSale) {
+                            var sale = makeSale(resultArr[index].sale);
+                            item.appendChild(sale);
+                        }
+                        row.appendChild(item);
+                        item.appendChild(makePicture(resultArr[index].picture));
+                        item.appendChild(heading);
+                        item.appendChild(price);
+                        item.appendChild(hiddenId);
+                    }
+
+                }
+            }
+        };
         if (resultArr.length < 5) {
             var row = createRow();
             resultDiv.appendChild(row);
             for (var k = 0; k < resultArr.length; k++, index++){
                 var item = createItem();
                 var heading = document.createElement('p');
-                var price = document.createElement('p');
+                var price = document.createElement('h3');
+                var hiddenId = document.createElement('h5');
+                hiddenId.innerHTML = resultArr[index].id;
                 heading.innerHTML = resultArr[index].name;
                 price.innerHTML = resultArr[index].price;
+                if (resultArr[index].isActiveSale) {
+                    var sale = makeSale(resultArr[index].sale);
+                    item.appendChild(sale);
+                }
                 row.appendChild(item);
                 item.appendChild(makePicture(resultArr[index].picture));
                 item.appendChild(heading);
                 item.appendChild(price);
+                item.appendChild(hiddenId);
             }
         }else {
-            for (var i = 0; i < 2; i++){
-                row = createRow();
-                resultDiv.appendChild(row);
-                for (var j = 0; j < 5; j++, index++){
-                    item = createItem();
-                    heading = document.createElement('p');
-                    price = document.createElement('p');
-                    heading.innerHTML = resultArr[index].name;
-                    price.innerHTML = resultArr[index].price;
-                    row.appendChild(item);
-                    item.appendChild(makePicture(resultArr[index].picture));
-                    item.appendChild(heading);
-                    item.appendChild(price);
+            makeTenItems();
+            buttonMore.onclick = function () {
+                makeTenItems();
+                console.log(index);
+                if (index >= resultArr.length) {
+                    buttonMore.style.display = 'none';
                 }
-            }
+            };
+
         }
-
-
-
-
-        console.log(index);
     }
 }
 
-/*function giveTenItems() {
-    if (document.getElementById('input').value) {
-        resultDiv.innerHTML = '';
-        var searchedValue = document.getElementById('input').value;
-        index = 0;
-        for (var i = 0; i < 2; i++){
-            var row = createRow();
-            resultDiv.appendChild(row);
-            for (var j = 0; j < 5; j++, index++){
+
+
+function makeSalesField() {
+    var index = 0;
+    var row = createRow();
+    biggestSalesDiv.appendChild(row);
+    var defaultSales =  function () {
+        for (var j = 0; j < 3; j++, index++){
+            if (biggestSales[index]) {
                 var item = createItem();
                 var heading = document.createElement('p');
-                var price = document.createElement('p');
-                heading.innerHTML = resultArr[index].name;
-                price.innerHTML = resultArr[index].price;
+                var price = document.createElement('h3');
+                var hiddenId = document.createElement('h5');
+                hiddenId.innerHTML = biggestSales[index].id;
+                heading.innerHTML = biggestSales[index].name;
+                price.innerHTML = biggestSales[index].price;
+                var sale = makeSale(biggestSales[index].sale);
+                item.appendChild(sale);
                 row.appendChild(item);
-                item.appendChild(makePicture(resultArr[index].picture));
+                item.appendChild(makePicture(biggestSales[index].picture));
                 item.appendChild(heading);
                 item.appendChild(price);
+                item.appendChild(hiddenId);
             }
         }
-        console.log(index);
-    }
-}*/
 
-/*function giveFiveItems() {
-    if (document.getElementById('input').value) {
-        resultDiv.innerHTML = '';
-        var row = createRow();
-        resultDiv.appendChild(row);
+    };
+    defaultSales();
+}
 
-        let index = 0;
-        var count = 0;
-        var searchedValue = document.getElementById('input').value;
-
-        while (data[index].name && count < 5) {
-            var str = data[index].name;
-            if (str.includes(searchedValue)) {
-                var item = createItem();
-                var heading = document.createElement('p');
-                var price = document.createElement('p');
-                heading.innerHTML = data[index].name;
-                price.innerHTML = data[index].price;
-                row.appendChild(item);
-                item.appendChild(makePicture(data[index].picture));
-                item.appendChild(heading);
-                item.appendChild(price);
-                count++;
-            }
-            index++;
-        }
-    }
-
-
-    // for (var j = 0; j < 5; j++) {
-    //     var item = createItem();
-    //     var heading = document.createElement('p');
-    //     var price = document.createElement('p');
-    //     heading.innerHTML = data[j].name;
-    //     price.innerHTML = data[j].price;
-    //     row.appendChild(item);
-    //     item.appendChild(makePicture(data[j].picture));
-    //     item.appendChild(heading);
-    //     item.appendChild(price);
-    // }
-}*/
-
-
+makeSalesField();
 
 function search(event) {
     event.preventDefault();
-    // giveFiveItems();
     makeArrayOfSearched();
     giveTenItems();
-    console.log(resultArr);
 }
 
 defaultPage();
-// giveFiveItems();
 
+
+shop.onclick = function (event) {
+    var target = event.target;
+    var clickedItem = target.closest('.item');
+    if (clickedItem) {
+        var childNodesLength = clickedItem.childNodes.length;
+        var itemId = clickedItem.childNodes[childNodesLength - 1].innerText;
+        alert( `Вы посмотрели товар: ${clickedItem.childNodes[childNodesLength - 3].innerText}, с ценой: ${clickedItem.childNodes[childNodesLength - 2].innerText}` );
+        if ( clickedId.indexOf(itemId) == -1 ) {
+            clickedId.push(itemId);
+            if (clickedId.length > 3) {
+                clickedId.splice(0, clickedId.length - 3);
+            }
+
+            makeWatchedArr();
+
+        }
+        makeWatched();
+    }
+    storing();
+};
+
+buttonSale.onclick = function () {
+    biggestSalesDiv.style.display = 'block';
+    watchedDiv.style.display = 'none';
+};
+
+buttonWatched.onclick = function () {
+    biggestSalesDiv.style.display = 'none';
+    watchedDiv.style.display = 'block';
+};
+
+var makeWatchedArr = function () {
+    for (var i = 0; i < data.length; i++) {
+        for (var j = 0; j < clickedId.length; j++){
+            if (data[i].id == clickedId[j]) {
+                if (!watchedArr.includes(data[i])) {
+                    watchedArr.push(data[i]);
+                }
+                if (watchedArr.length > 3) {
+                    watchedArr.splice(0, watchedArr.length - 3);
+                }
+            }
+        }
+    }
+};
+
+var makeWatched = function () {
+    watchedDiv.innerHTML = '';
+    var row = createRow();
+    watchedDiv.appendChild(row);
+    for (var j = 0; j < 3; j++){
+        if (watchedArr[j]) {
+            var item = createItem();
+            var heading = document.createElement('p');
+            var price = document.createElement('h3');
+            var hiddenId = document.createElement('h5');
+            hiddenId.innerHTML = watchedArr[j].id;
+            heading.innerHTML = watchedArr[j].name;
+            price.innerHTML = watchedArr[j].price;
+            var sale = makeSale(watchedArr[j].sale);
+            item.appendChild(sale);
+            row.appendChild(item);
+            item.appendChild(makePicture(watchedArr[j].picture));
+            item.appendChild(heading);
+            item.appendChild(price);
+            item.appendChild(hiddenId);
+        }
+    }
+};
+
+function storing() {
+    localStorage.setItem('clickedId', JSON.stringify(clickedId));
+}
+
+var retrievedData = localStorage.getItem('clickedId');
+var newClickedId = JSON.parse(retrievedData);
+if(newClickedId){
+    for(var i = 0; i < newClickedId.length; i++){
+        clickedId.push(newClickedId[i])
+    }
+}
+
+makeWatchedArr();
+makeWatched();
